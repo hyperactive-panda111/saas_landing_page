7d23bfe32cdda4215ffda1310e3a42963143f9ac
Yaw Amo Mensah <amomensah300@gmail.com>
2024-10-06 17:57:22 +0100

    added Header component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,9 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from '@/assets/arrow-right.svg';
+
+export const Header = () => {
+  return (
+    <div className='flex justify-center items-center py-3 bg-black text-white text-sm'>
+      <p>Get started for free</p>
+    </div>
+  );
 };
