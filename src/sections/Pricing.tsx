64a950895556ed9f0b7d2da81f54e53aae194e97
amomensah675 <amomensah675@gmail.com>
2024-10-07 13:52:03 +0000

    added new tailwind classes

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -55,7 +55,7 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section className="py-24 ">
+    <section className="py-24 bg-white">
       <div className="container">
         <div className='section-heading'>
           <h2 className="section-title">Pricing</h2>
@@ -73,7 +73,7 @@ export const Pricing = () => {
               inverse, 
               features
             }) => (
-          <div key={title} className={twMerge('p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full', 
+          <div key={title} className={twMerge('card', 
           inverse && 'border-black bg-black text-white')}>
             <div className='flex justify-between'>
               <h3 className={twMerge('text-lg font-bold text-black/50', 
