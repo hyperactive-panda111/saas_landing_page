562c19660454e2f70fedb6d6badd2e23e82872e6
amomensah675 <amomensah675@gmail.com>
2024-10-07 09:41:58 +0000

    added ProductShowcase component

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,47 @@
+import productImage from '@/assets/product-image.png';
+import pyramidImage from '@/assets/pyramid.png';
+import tubeImage from '@/assets/tube.png';
+
+import Image from 'next/image';
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className='overflow-x-clip bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24'>
+      <div className="container">
+        <div className='max-w-[540px] mx-auto'>
+          <div className='flex justify-center'>
+            <div className='tag'>Boost your productivity</div>
+          </div>
+          <h2 className='text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5'>
+            A more effective way to track progress
+          </h2>
+          <p className='text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5'>
+            Effortlessly turn your ideas into a fully functional, responsive, 
+            SaaS ebsite in just minutes with this template.
+          </p>
+        </div>
+        <div className='relative'>
+          <Image
+            src={productImage}
+            alt='Product Image'
+            className='mt-10'
+          />
+          <Image
+            src={pyramidImage}
+            alt='Pyramid Image'
+            className='hidden md:block absolute -right-36 -top-32'
+            height={262}
+            width={262}
+          />
+          <Image
+            src={tubeImage}
+            alt='Tube Image'
+            className='hidden md:block absolute bottom-24 -left-36'
+            height={262}
+            width={262}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
